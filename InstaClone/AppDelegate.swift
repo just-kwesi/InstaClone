61a7e8973aca91275310f4f6d809167941483244
Frederick Tetteh <kwesiossom@gmail.com>
2020-10-23 23:47:48 -0400

    user stories completed

--- InstaClone/AppDelegate.swift
+++ InstaClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "DhlQ2kqrwLSRfcJoeBod5y3Dhcgcl94zpjL2BLAb" // <- UPDATE
+                    $0.clientKey = "VuI6zOkpY0ASYOxjKayblOySH401PaTMTH3ncqjE" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
